9dbdabd5fafd460d1903c8c04161119379b10f2b
ajpineda.cca <ajpineda23-0300@cca.edu.ph>
2025-02-12 17:59:28 +0800

    Update index.php
    For example

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <html>
-        <head>
-    <title></title>
+    <head>
+        <title></title>
         </head>
-         <body>
+     <body>
         <h1>
-            
-         <?php echo "Hello World"; ?>
-        
+        <?php echo "hello World"; ?> <br>
+        <?php
+            $variable1 = 10;
+            $variable2 = 5;
+        ?>
+        Num1: <?php echo $variable1; ?> <br>
+        Num2: <?php echo $variable2; ?> <br>
+        Sum of Num1 and Num2: <?php echo $variable1 + $variable2; ?> <br>
         </h1>
-        </body>
+    </body>
 </html>
